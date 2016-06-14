f2c18b5877fb2537802c7db7d32602d26b681005
Jared Roth <jared.roth@gmail.com>
2016-06-14 10:02:05 -0600

    Second story

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,14 @@ class Api::V1::ItemsController < ApplicationController
   def index
     respond_with Item.select(:id, :name, :description, :image_url)
   end
+
+  def show
+    respond_with Item.select(:id, :name, :description, :image_url).find(params[:id])
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.delete
+    respond_with item
+  end
 end
--- spec/controllers/api/v1/item_controller_spec.rb
+++ spec/controllers/api/v1/item_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Api::V1::ItemsController, type: :controller do
     get :index, format: :json
     parsed_json = JSON.parse(response.body)
 
-    assert_response :success
+    assert_response 200
 
     expect(parsed_json.count).to eq(2)
     expect(parsed_json.first["name"]).to eq("Jeans")
@@ -15,5 +15,33 @@ RSpec.describe Api::V1::ItemsController, type: :controller do
     expect(parsed_json.first["image_url"]).to eq("http://www.google.com")
     expect(parsed_json.first["created_at"]).to eq(nil)
     expect(parsed_json.first["updated_at"]).to eq(nil)
+  end
+
+  it "#show" do
+    create(:item)
+    id = Item.first.id
+
+    get :show, id: id, format: :json
+    parsed_json = JSON.parse(response.body)
+
+    assert_response 200
+
+    expect(parsed_json["id"]).to eq(id)
+    expect(parsed_json["name"]).to eq("Jeans")
+    expect(parsed_json["description"]).to eq("They be tight")
+    expect(parsed_json["image_url"]).to eq("http://www.google.com")
+    expect(parsed_json["created_at"]).to eq(nil)
+    expect(parsed_json["updated_at"]).to eq(nil)
+  end
+
+  it "#destroy" do
+    create(:item)
+    id = Item.first.id
+
+    delete :destroy, id: id
+
+    assert_response 204
+
+    expect(parsed_json.count).to eq(0)
   end
 end
