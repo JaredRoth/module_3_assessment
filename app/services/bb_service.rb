4c2b839de04a934308d3af4c5a4dbc45d1434b10
Jared Roth <jared.roth@gmail.com>
2016-06-14 11:00:59 -0600

    WIP BB API

--- app/services/bb_service.rb
+++ app/services/bb_service.rb
@@ -2,11 +2,10 @@ class BBService
   def initialize(params)
     @params = params
     @connection = Faraday.new(url: "https://api.bestbuy.com/")
-    @connection.headers["X-Api-Key"] = ENV['BB_KEY']
   end
 
   def search_by_zip
-    response = @connection.get "stores(postalCode=#{@params[:zip]},25)?format=json&show=longName,city,distance,phone,storeType&apiKey=#{ENV['BB_KEY']}"
+    response = @connection.get "v1/stores(postalCode=#{@params[:zip]},25)?format=json&show=longName,city,distance,phone,storeType&apiKey=#{ENV['BB_KEY']}"
     JSON.parse(response.body, symbolize_names: true)
   end
 end
